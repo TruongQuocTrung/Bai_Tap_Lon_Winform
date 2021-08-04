d6e5867f7247a6d84f82314d993c35953daa264a
nguyenvantung110 <bongbong27051998@gmail.com>
2021-08-04 20:30:47 +0700

    Add chuc nang tac gia va the loai

--- Bai_Tap_Lon_Winform/frmThemTacGia.cs
+++ Bai_Tap_Lon_Winform/frmThemTacGia.cs
@@ -21,5 +21,63 @@ namespace Bai_Tap_Lon_Winform
         {
             this.Close();
         }
+
+        private void txtTim_Click(object sender, EventArgs e)
+        {
+            txtTim.Clear();
+            txtTim.ForeColor = Color.Black;
+        }
+        TacGiaProcessing tacGia = new TacGiaProcessing();
+        private void frmThemTacGia_Load(object sender, EventArgs e)
+        {
+            GridViewTacGia.DataSource = tacGia.showTG();
+        }
+
+        private void GridViewTacGia_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = GridViewTacGia.SelectedCells[0].RowIndex;
+            txtMaTG.Text = GridViewTacGia.Rows[index].Cells[0].Value.ToString();
+            txtTenTG.Text = GridViewTacGia.Rows[index].Cells[1].Value.ToString();
+            txtLienHe.Text = GridViewTacGia.Rows[index].Cells[2].Value.ToString();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            tacGia.addTacGia(txtMaTG.Text, txtTenTG.Text,txtLienHe.Text);
+            frmThemTacGia_Load(sender, e);
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            txtMaTG.Clear();
+            txtTenTG.Clear();
+            txtLienHe.Clear();
+        }
+
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            tacGia.editTacGia(txtMaTG.Text, txtTenTG.Text, txtLienHe.Text);
+            frmThemTacGia_Load(sender, e);
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+
+            tacGia.xoaTacGia(txtMaTG.Text);
+            frmThemTacGia_Load(sender, e);
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            if (txtTim.Text.Trim().Length > 0)
+            {
+                GridViewTacGia.DataSource = tacGia.timTheLoai(txtTim.Text);
+                GridViewTacGia.ClearSelection();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng kiểm tra lại mã tác giả", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
     }
 }
